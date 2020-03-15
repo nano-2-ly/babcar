4a496d949b4fa458a0c13a236f49de341eee44d5
nanotoly <dong991207@unist.ac.kr>
2020-03-15 21:39:38 +0900

    ocr test complete

---
+++ main.py
@@ -0,0 +1,78 @@
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+def detect_text(path):
+        """Detects text in the file."""
+        from google.cloud import vision
+        import io
+        client = vision.ImageAnnotatorClient()
+
+        with io.open(path, 'rb') as image_file:
+            content = image_file.read()
+
+        image = vision.types.Image(content=content)
+
+        response = client.text_detection(image=image)
+        texts = response.text_annotations
+        print('Texts:')
+
+        for text in texts:
+            print('\n"{}"'.format(text.description))
+
+            vertices = (['({},{})'.format(vertex.x, vertex.y)
+                        for vertex in text.bounding_poly.vertices])
+
+            print('bounds: {}'.format(','.join(vertices)))
+
+        if response.error.message:
+            raise Exception(
+                '{}\nFor more info on error messages, check: '
+                'https://cloud.google.com/apis/design/errors'.format(
+                    response.error.message))
+def detect_document(path):
+        """Detects document features in an image."""
+        from google.cloud import vision
+        import io
+        client = vision.ImageAnnotatorClient()
+
+        with io.open(path, 'rb') as image_file:
+            content = image_file.read()
+
+        image = vision.types.Image(content=content)
+
+        response = client.document_text_detection(image=image)
+
+        for page in response.full_text_annotation.pages:
+            for block in page.blocks:
+                print('\nBlock confidence: {}\n'.format(block.confidence))
+
+                for paragraph in block.paragraphs:
+                    print('Paragraph confidence: {}'.format(
+                        paragraph.confidence))
+
+                    for word in paragraph.words:
+                        word_text = ''.join([
+                            symbol.text for symbol in word.symbols
+                        ])
+                        print('Word text: {} (confidence: {})'.format(
+                            word_text, word.confidence))
+
+                        for symbol in word.symbols:
+                            print('\tSymbol: {} (confidence: {})'.format(
+                                symbol.text, symbol.confidence))
+
+        if response.error.message:
+            raise Exception(
+                '{}\nFor more info on error messages, check: '
+                'https://cloud.google.com/apis/design/errors'.format(
+                    response.error.message))
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+# The name of the image file to annotate
+file_name = os.path.abspath('./resources/test.jpg')
+detect_text(file_name)
